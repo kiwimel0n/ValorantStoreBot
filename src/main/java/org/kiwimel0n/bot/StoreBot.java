519f9f7c9faa147ecfa07a311c0cff0f781f20b5
kiwimel0n <alstkd8580@naver.com>
2025-04-29 12:48:11 +0900

    환경 초기화

---
+++ src/main/java/org/kiwimel0n/bot/StoreBot.java
@@ -0,0 +1,13 @@
+package org.kiwimel0n.bot;
+
+import net.dv8tion.jda.api.JDABuilder;
+
+public class StoreBot {
+
+    public static void main(String[] args) {
+
+        JDABuilder.createDefault("토큰값")
+                .build();
+
+    }
+}
